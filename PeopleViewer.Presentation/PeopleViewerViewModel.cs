33148716138adfdbbf75a278448f777110c923cf
María Pedreira <mjpedreira1@hotmail.com>
2018-11-01 08:43:08 -0700

    Add project files.

---
+++ PeopleViewer.Presentation/PeopleViewerViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleViewer.Presentation
+{
+    public class PeopleViewerViewModel : INotifyPropertyChanged
+    {
+        protected ServiceRepository Repository;
+        private IEnumerable<Person> _people;
+        public IEnumerable<Person> People
+        {
+            get { return _people; }
+            set
+            {
+                if (_people == value)
+                    return;
+                _people = value;
+                RaisePropertyChanged("People");
+            }
+        }
+
+        public PeopleViewerViewModel()
+        {
+            Repository = new ServiceRepository();
+        }
+
+        #region Commands
+
+        #region RefreshCommand Standard Stuff
+        #endregion RefreshCommand Standard Stuff
+
+        public void Execute(object parameter)
+        {
+            ViewModel.People = ViewModel.Repository.GetPeople();
+        }
+
+
+        #region ClearCommand Standard Stuff
+        public void Execute(object parameter)
+        {
+            ViewModel.People = new List<Person>();
+        }
+
+        #endregion ClearCommand Standard Stuff
+
+
+
+        #endregion Commands
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string properyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion INotifyPropertyChanged Members
+
+    }
+}
---
+++ PersonRepository.Service/ServiceRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PeopleViewer.SharedObjects;
+
+namespace PersonRepository.Service
+{
+    public class ServiceRepository
+    {
+        PersonServiceClient ServiceProxy = new PersonServiceClient();
+        public IEnumerable<Person> GetPeople()
+        {
+            return ServiceProxy.GetPeople();
+        }
+        public Person GetPerson(string lastName) { }
+        public void AddPerson(Person newPerson) { }
+        public void DeletePerson(string lastName) { }
+        public void UpdatePeople(IEnumerable<Person> updatedPeople) { }
+
+    }
+}
